ac41dbcb1285fbf27f0e93cbb61e4c1839ba7ec8
NguyenTungSon0503 <sonmeliodas0503@gmail.com>
2023-12-06 16:08:31 +0700

    Update: Handle API error

--- server/src/controllers/companies.controller.js
+++ server/src/controllers/companies.controller.js
@@ -1,22 +1,19 @@
 import prisma from '../service/prisma.js';
-import hashPasswordFunction from '../utils/hash.js';
-
-const handleErrorResponse = (res, error, statusCode) => {
-  console.error(error);
-  res.status(statusCode).send('Internal Server Error');
-};
+import ApiError from '../utils/ApiError.js';
+import { StatusCodes } from 'http-status-codes';
 
 const companyController = {
-  getAll: async (req, res) => {
+  getAll: async (req, res, next) => {
     try {
       const allCompanies = await prisma.company.findMany({});
       return res.status(200).json(allCompanies);
     } catch (error) {
-      handleErrorResponse(res, error, 500);
+      next(error);
+      throw new ApiError(StatusCodes.INTERNAL_SERVER_ERROR, error.message);
     }
   },
 
-  getCompanyById: async (req, res) => {
+  getCompanyById: async (req, res, next) => {
     const { id } = req.params;
     const idInt = parseInt(id);
     try {
@@ -27,11 +24,12 @@ const companyController = {
         ? res.status(200).json(company)
         : res.status(404).json({ message: `Company ${id} not found` });
     } catch (error) {
-      handleErrorResponse(res, error, 500);
+      next(error);
+      throw new ApiError(StatusCodes.INTERNAL_SERVER_ERROR, error.message);
     }
   },
 
-  create: async (req, res) => {
+  create: async (req, res, next) => {
     const { email, website, phone_number, hust_partner, logo_url, address } =
       req.body;
     try {
@@ -47,11 +45,12 @@ const companyController = {
       });
       return res.status(201).json(createdCompany);
     } catch (error) {
-      handleErrorResponse(res, error, 500);
+      next(error);
+      throw new ApiError(StatusCodes.INTERNAL_SERVER_ERROR, error.message);
     }
   },
 
-  delete: async (req, res) => {
+  delete: async (req, res, next) => {
     const { id } = req.params;
     const idInt = parseInt(id);
 
@@ -72,11 +71,12 @@ const companyController = {
         message: `Company ${id} deleted successfully`,
       });
     } catch (error) {
-      return res.status(500).json({ message: error.meta.cause });
+      next(error);
+      throw new ApiError(StatusCodes.INTERNAL_SERVER_ERROR, error.message);
     }
   },
 
-  update: async (req, res) => {
+  update: async (req, res, next) => {
     const { id } = req.params;
     const idInt = parseInt(id);
 
@@ -99,7 +99,8 @@ const companyController = {
         updatedCompany,
       });
     } catch (error) {
-      return res.status(500).json({ message: error.meta.cause });
+      next(error);
+      throw new ApiError(StatusCodes.INTERNAL_SERVER_ERROR, error.message);
     }
   },
 };
