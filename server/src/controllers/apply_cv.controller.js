fb06510e85d7cd2c3c8309057dafb56a7da7ffe3
Nguyen Duan <87711300+DuanNguyen2907@users.noreply.github.com>
2023-12-26 16:27:14 +0700

    Api get applyJobs

--- server/src/controllers/apply_cv.controller.js
+++ server/src/controllers/apply_cv.controller.js
@@ -51,6 +51,49 @@ const applyController = {
       throw new ApiError(StatusCodes.INTERNAL_SERVER_ERROR, error.message);
     }
   },
+  getJobById: async (req, res, next) => {
+    const { id } = req.params;
+    const idInt = parseInt(id);
+    try {
+      const job = await prisma.job.findUnique({
+        where: { id: idInt },
+        include: {
+          company: {
+            select: {
+              name: true,
+              hust_partner: true,
+              logo_url: true,
+              website: true,
+              phone_number: true,
+              email: true,
+            },
+          },
+          majors :{
+            select: {
+              name: true,
+            }
+          },
+          jobTypeRelations: {
+            select: {
+              type: {
+                select: {
+                  name: true,
+                },
+              },
+            },
+          },
+        }
+      });
+      if(job) {
+        return res.status(200).json(job);
+      } else {
+        return res.status(404).json({ message: `Job ${id} not found` });
+      }
+    } catch (error) {
+      next(error);
+      throw new ApiError(StatusCodes.INTERNAL_SERVER_ERROR, error.message);
+    }
+  },
 };
 
 export default applyController;
